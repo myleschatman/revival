4a46cc99bf8738dc5d78d0c08ea495bcbe7f9a97
Myles Chatman <myleschatman@gmail.com>
2018-12-28 11:29:17 -0800

    Added responsive layout and placeholder images

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,6 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+const browserSt=ync = require('browser-sync').create();
 
-gulp.task('vendor', function() {
-  // Bootstrap
-  gulp.src([
-    './node_modules/bootstrap/dist/**/*',
-    '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-    '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-  ])
-  .pipe(gulp.dest('./vendor/bootstrap'))
+gulp.task('libs', function() {
 
-  // jQuery
-  gulp.src([
-    './node_modules/jquery/dist/*',
-    '!./node_modules/jquery/dist/core.js'
-  ])
-  .pipe(gulp.dest('./vendor/jquery'))
-});
-
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-});
-
-gulp.task('serve', ['browserSync'], function() {
-  gulp.watch('./css/*.css', browserSync.reload);
-  gulp.watch('./*.html', browserSync.reload);
-});
-
-gulp.task('default', ['vendor', 'serve']);
+})
